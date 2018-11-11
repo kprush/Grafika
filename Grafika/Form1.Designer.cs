86daeaf542b07e410917d83c9cb5bb4b78562bed
Konstantin Panov <kostia.panov8v@gmail.com>
2018-11-11 14:13:44 +0100

    graphic program

---
+++ Grafika/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Grafika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel10.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(14, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(20, 20);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(40, 16);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(20, 20);
+            this.panel2.TabIndex = 1;
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(66, 16);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(20, 20);
+            this.panel3.TabIndex = 1;
+            this.panel3.Click += new System.EventHandler(this.panel3_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Location = new System.Drawing.Point(92, 16);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(20, 20);
+            this.panel4.TabIndex = 4;
+            this.panel4.Click += new System.EventHandler(this.panel4_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Location = new System.Drawing.Point(118, 16);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(20, 20);
+            this.panel5.TabIndex = 3;
+            this.panel5.Click += new System.EventHandler(this.panel5_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Location = new System.Drawing.Point(144, 16);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(20, 20);
+            this.panel6.TabIndex = 2;
+            this.panel6.Click += new System.EventHandler(this.panel6_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel7.Location = new System.Drawing.Point(170, 16);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(20, 20);
+            this.panel7.TabIndex = 7;
+            this.panel7.Click += new System.EventHandler(this.panel7_Click);
+            // 
+            // panel8
+            // 
+            this.panel8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel8.Location = new System.Drawing.Point(196, 16);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(20, 20);
+            this.panel8.TabIndex = 6;
+            this.panel8.Click += new System.EventHandler(this.panel8_Click);
+            // 
+            // panel9
+            // 
+            this.panel9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel9.Location = new System.Drawing.Point(222, 16);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(20, 20);
+            this.panel9.TabIndex = 5;
+            this.panel9.Click += new System.EventHandler(this.panel9_Click);
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.panel3);
+            this.panel10.Controls.Add(this.panel7);
+            this.panel10.Controls.Add(this.panel1);
+            this.panel10.Controls.Add(this.panel8);
+            this.panel10.Controls.Add(this.panel6);
+            this.panel10.Controls.Add(this.panel9);
+            this.panel10.Controls.Add(this.panel2);
+            this.panel10.Controls.Add(this.panel4);
+            this.panel10.Controls.Add(this.panel5);
+            this.panel10.Location = new System.Drawing.Point(-2, -4);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(263, 49);
+            this.panel10.TabIndex = 8;
+            this.panel10.Paint += new System.Windows.Forms.PaintEventHandler(this.panel10_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 450);
+            this.Controls.Add(this.panel10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.panel10.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+    }
+}
+
---
+++ Grafika/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafika
+{
+    public partial class Form1 : Form
+    {
+        bool rys;
+        Graphics g;
+        Pen p = new Pen(Color.Black, 2);
+        Pen blue = new Pen(Color.Blue, 2);
+        Pen black = new Pen(Color.Black, 2);
+        Pen red = new Pen(Color.Red, 2);
+        Pen gray = new Pen(Color.Gray, 2);
+        Pen crimson = new Pen(Color.Crimson, 2);
+        Pen orange = new Pen(Color.Orange, 2);
+        Pen yellow = new Pen(Color.Yellow, 2);
+        Pen lightblue = new Pen(Color.LightBlue, 2);
+        Pen purple = new Pen(Color.Purple, 2);
+        int x0, y0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            panel1.BackColor = Color.Black;
+            panel2.BackColor = Color.Gray;
+            panel3.BackColor = Color.Crimson;
+            panel4.BackColor = Color.Red;
+            panel5.BackColor = Color.Orange;
+            panel6.BackColor = Color.Yellow;
+            panel7.BackColor = Color.Green;
+            panel8.BackColor = Color.Blue;
+            panel9.BackColor = Color.Purple;
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            rys = true;
+            x0 = e.X;
+            y0 = e.Y;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            rys = false;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Black, 2);
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Gray, 2);
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Crimson, 2);
+        }
+
+        private void panel4_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Red, 2);
+        }
+
+        private void panel5_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Orange, 2);
+        }
+
+        private void panel6_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Yellow, 2);
+        }
+
+        private void panel7_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Green, 2);
+        }
+
+        private void panel8_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Blue, 2);
+        }
+
+        private void panel9_Click(object sender, EventArgs e)
+        {
+            p = new Pen(Color.Purple, 2);
+        }
+
+        private void panel10_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(rys)
+            {
+                int x = e.X; int y = e.Y;
+                g.DrawLine(p, x0, y0, x, y);
+                x0 = x;y0 = y;
+            }
+        }
+    }
+}
